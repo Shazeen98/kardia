71e9d7547faa1fc5928e956b87cdc321876b1ce9
Yeheni99 <66565503+Yeheni99@users.noreply.github.com>
2021-02-23 16:19:55 +0530

    Add files via upload

---
+++ GUI/src/components/Pages/Predict.js
@@ -0,0 +1,14 @@
+  
+import './Predict.css';
+
+function Predict() {
+  return (
+    <div className="App">
+      <div className="topic"> Predict Your Morality </div>
+      <img src="/images/predict.jpeg" alt=""/>
+      
+    </div>
+  );
+}
+
+export default Predict;
