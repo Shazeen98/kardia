95c8fa740fbc7659aaa48dd7d473fb1b2f2a1366
ThreshaK <66517902+ThreshaK@users.noreply.github.com>
2021-02-28 10:31:00 +0530

    initial commit

--- GUI/src/components/Form1.js
+++ GUI/src/components/Form1.js
@@ -1,29 +1,39 @@
 
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './Form1.css'
-import { Button } from './Button';
 
 
 function Form1(){
     return(
         <div className = " form-start">
-        
-        <div className = 'image2'>
-            <img src="/Images/img2.png" width='600px' height= '600px'/>   
-        </div>
-         <h1 className='log-in'>Login</h1>
-         <p className = 'log-text'>Sign in to your account to continue</p>
-         <div className = 'list1'>
-         <form className = 'info'>
-           
-            <label>Email Address</label><br/>
-            <input type="text" placeholder='name@yourdomain.com'/><br/><br/><br/>
-            <label>Password</label><br/>
-            <input type="password" placeholder='Enter your password'/><br/><br/><br/>
-            <button>Sign in</button>
-        
-        </form>
-        </div>
+            <Link to = '/home'>
+            <i className = " fas fa-times"/> 
+            </Link>
+            <div className = 'image2'>
+                <img src="/Images/img2.png" width='1258px' height= '964px'/>   
+            </div>
+            <div className='log-cont'>
+            <h1 className='log-in'>Login</h1>
+            <button className = 'fb-sign-in'>Continue with facebook</button><br/>
+                        <div className = "or-text1">
+                        ──────────────── or ───────────────
+                        </div>
+                       
+            <p className = 'log-text'>Sign in to your account to continue</p>
+                <div className = 'list1'>
+                    <form className = 'cont'>
+                    <label className="lbl">Email Address</label><br/><br/>
+                    <input className = 'email-txt' type="text" placeholder= '     Enter your email'/><br/><br/><br/>
+                    <label className="lbl">Password</label><br/><br/>
+                    <input className = 'pswd-txt'type="password" placeholder='     Enter your password'/><br/><br/><br/>
+                    <button className = 'btn-sign'>Sign in</button>
+                    <p className = 'p-reg'>Not registered ?<Link to = '/signup' className = "create-acc">  Create account</Link></p>
+            
+                    
+                    </form>
+                </div>
+            </div>
         </div>
     );
 }
