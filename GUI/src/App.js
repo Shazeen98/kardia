5460935bb9fbdcd9405db56d8ff686c5f3227e57
ThreshaK <66517902+ThreshaK@users.noreply.github.com>
2021-02-28 10:29:59 +0530

    initial commit

--- GUI/src/App.js
+++ GUI/src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import Navbar from './components/Navbar';
 import './App.css';
-import Home from './components/Pages/Home'
-import Getstarted from './components/Pages/Getstarted';
-import Predict from './components/Pages/Predict';
+import Mainpage from './components/Pages/Mainpage'
 import Login from './components/Pages/Login'
 
 
+
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Getstarted from './components/Pages/Getstarted';
+import Homepage from './components/Pages/Homepage';
+
 
 function App() {
   return (
     <>
       <Router>
-        <Navbar />
-        
         <Switch>
-          <Route path='/' exact component={Home}/>
+          <Route path='/' exact component={Mainpage}/>
           <Route path= '/log-in' component={Login}/>
           <Route path = '/sign-up' component = {Getstarted}/>
-          <Route path = '/predict' component = {Predict}/>
-          
+          <Route path = '/home' component = {Homepage}/>
         </Switch>
       </Router>
     </>
